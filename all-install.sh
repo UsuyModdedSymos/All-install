fb11f436b3050e4f96e7cb1abbe3e1c2e6f2c023
Mr Malware.404 <46353843+UsuyModdedSymos@users.noreply.github.com>
2019-05-09 16:01:07 +0700

    Update all-install.sh

--- all-install.sh
+++ all-install.sh
@@ -1,5 +1,30 @@
 #! /data/data/com.termux/files/usr/bin/bash
 #---All--In--One---Termux---Package-------
+echo "==================================="
+    sleep 2
+    echo "====  All-install  ===="
+    sleep 0.7
+    echo "==================================="
+
+clear
+
+echo "(:"Selamat datang Bro !!":)"
+echo "(:"Di Tool Mamang Ganteng":)" 
+echo "(:"Tulis nick Kalian di bawah":)"
+echo
+
+#tulisan keluar
+read nick #membaca yang ditulis
+clear
+
+clear
+echo "(:" Selamat datang $nick ":)"
+sleep 9
+echo "(:" Tunggu sekitar 15-20 menit ":)"
+echo "(:"DONT CLOSE BEFORE DONE":)"
+
+
+echo
 apt update -y #update Termux
 apt upgrade #upgrade Termux
 apt install figlet -y #Text-design
@@ -11,31 +36,38 @@ echo "" #blank
 echo "" #blank
 #logo of this script
 figlet "All-install"
-echo "---by UsuyModdedSymos---"
-echo "---Creator: Hirak Das--"
+echo "---Mod UsuyModdedSymos---"
+echo "---The Cyber Symos Team--"
 echo "---Spesial tank: hackMobi--"
 echo "---Version: 1.0----"
 echo "" #blank
+sleep 2
+sleep 9
 
 #language
 apt install python -y
 apt install python2 -y
 apt install perl -y
-apt install c -y
-apt install c++ -y
+apt install nodejs -y
+apt install php -y
+apt install ruby -y
 #common pkg
 apt install git -y
+apt install nodejs -y
+apt install worlist -y
 apt install nano -y
 apt install vim -y
+apt install openssh -y
 apt install wget -y
+apt install ssh -y
 apt install curl -y
 apt install zip -y
 apt install tar -y
 apt install less -y
-apt install ruby -y
 apt install figlet -y
 apt install toilet -y
-apt install php -y
+apt install tsu -y
+apt install mpv -y
 apt install cowsay -y
 #testing tools
 apt install nmap -y
@@ -43,18 +75,45 @@ apt install hydra -y
 apt install apache2
 gem install lolcat
 #pip pkg
+pip install --upgrade pip
+pip install beautifulsoup4
+pip install mechanize
+pip install google
 pip install colorama
 pip install termcolor
+pip install parse
+pip install wordlist
+pip install pexpect
+pip install pycrypto
+pip install requests
+pip install pyopenssl
+pip install pefile
+pip install impacket
+#Generate QR Codes
+pip install qrcode
+pip install pillow
 #pip2 pkg
 pip2 install beautifulsoup4
 pip2 install mechanize
 pip2 install requests
+pip install google
+pip install colorama
+pip install termcolor
+pip install parse
+pip install wordlist
 
-
-
+echo "============================="
+    sleep 2
+    echo "         ==   All-install   =="
+    sleep 2
+    echo "=================================="
+echo "[✔] All is done! [✔]"
+echo "[✔] Now You can run this module [✔]"
+     sleep 9
 #end
 echo "####################################################################"
 echo "--------------------------------------------------------------------"
 echo "---------SUCCESSFULY INSTALLED--------------------------------------"
 echo "--------------------------------------------------------------------"
 echo "###################################################################"
+###########################"
